9f7dc998a6d568989ca01fd5c5c0acfac0f7e0d4
Martin Carlin <martin@martincarlin.uk>
2019-02-05 20:32:05 +0000

    fix: update app.js

--- content/themes/carlin/assets/js/app.js
+++ content/themes/carlin/assets/js/app.js
@@ -18,7 +18,7 @@ var App = function () {
             var valid = 1;
 
             $form.find('.required').each(function() {
-                if ($(this).val() == '') {
+                if ($(this).val() === '') {
                     $(this).parent().addClass('is-error');
                     valid = 0;
                 } else {
@@ -29,37 +29,12 @@ var App = function () {
 
             if (valid == 1) {
 
-                console.log('Sending...');
                 $submit.addClass('is-loading is-disabled');
 
                 $('div.notification').replaceWith('<div class="notification is-primary">' +
                                             '<button class="delete"></button>' +
                                             '<i class="fa fa-info-circle"></i> Processing form...' +
                                           '</div>');
-
-                $.ajax({
-                    url: "https://formspree.io/martin@martincarlin.uk", 
-                    method: "POST",
-                    data: $form.serialize(),
-                    dataType: "json",
-                    success: function(data) {
-                        console.log('Email Sent!');
-                        $('div.notification').replaceWith('<div class="notification is-success">' +
-                                                    '<button class="delete"></button>' +
-                                                    '<i class="fa fa-check"></i> Thank you, your email has been sent' +
-                                                  '</div>');
-
-                        $form.addClass('is-hidden').remove();
-                    },
-                    error: function() {
-                        console.log('Error!');
-                        $('div.notification').replaceWith('<div class="notification is-danger">' +
-                                            '<button class="delete"></button>' +
-                                            '<i class="fa fa-exclamation-circle"></i> An error occurred, please try again' +
-                                          '</div>');
-                    }  
-                });
-
 
             } else {
                 console.log('Invalid');
